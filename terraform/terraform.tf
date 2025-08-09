9f50c4e513cd3734d87b2ec43b21750b580a03cf
Arun4Rajbhar <aruanamika7275@gmail.com>
2025-08-09 19:32:18 +0530

    adding this file

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "state-tf-bucket-987"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 
   required_version = "~> 1.6.3"
